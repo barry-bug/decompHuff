e5ce7a01d9501df511d088d8216a58e9d6249c99
barry-bug <72456359+barry-bug@users.noreply.github.com>
2021-06-09 00:35:44 +0200

    Update Compresser.java

--- Compresser.java
+++ Compresser.java
@@ -16,3 +16,27 @@ public class Compresser {
 	        codingmap = new HashMap<>();
 	        Huffmancoding(racine);
 	    }
+	
+		    /*Méthode permettant de faire le codage de huffman à partir du noeud racine de l'arbre : en ajoutant 1 vers le noeud fils droit et 0 vers le noeud fils gauche  */
+	    /*La méthode est récusrive car on l'utilisera sur chaque fils du noeud prix en paramétre jusqu'à atteindre l'extrémité. Ce sera cette succession de 1 et de 0 qui sera 
+	     le code de huffman. Chaque caractère aura sa propre suite de bits.*/
+	    private void Huffmancoding(Noeud myRoot) {
+	    	//Traverse through tree adding 1s for right, 0s for left.
+	        if (myRoot.getLeftnode() != null) {
+	            codage.append(0);
+	            Huffmancoding(myRoot.getLeftnode());
+	            codage.deleteCharAt(codage.length() - 1);
+	        }
+
+	        if (myRoot.getCaract() != null) {
+	            String str = String.valueOf(codage);
+	            codingmap.put(myRoot.getCaract(), str);
+	        }
+
+	        if (myRoot.getRightnode() != null) {
+	            codage.append(1);
+	            Huffmancoding(myRoot.getRightnode());
+	            codage.deleteCharAt(codage.length() - 1);
+	        }
+
+	    }
