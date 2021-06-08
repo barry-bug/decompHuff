7351b8e929380294b04ace7905d8531f72bb968f
barry-bug <72456359+barry-bug@users.noreply.github.com>
2021-06-09 00:30:32 +0200

    Update Frequencechar.java

--- Frequencechar.java
+++ Frequencechar.java
@@ -21,7 +21,7 @@ public class FrequenceChar {
 		Deffeuilles(); 
 	}
 	
-		/* On crée un table de caractères du texte donné en comptant pour chacun d'entre eux sa fréquence d'apparition */
+	/* On crée un table de caractères du texte donné en comptant pour chacun d'entre eux sa fréquence d'apparition */
 	private void Frequencecaract() {
 		char[] table = text.toCharArray();
 		for (char car : table) {
@@ -40,6 +40,25 @@ public class FrequenceChar {
 		}
 	}
 	
-	
-		/*Getter et setter*/
+	/*Getter et setter*/
 
+
+	/* retourne le table des fréquences des caractères*/
+	public Map<Character, Integer> getFrequences() {
+		return frequences;
+	}
+
+	public void setFrequences(Map<Character, Integer> frequences) {
+		this.frequences = frequences;
+	}
+
+	public List<Noeud> getFeuilles() {
+		return feuilles;
+	}
+
+	public void setFeuilles(List<Noeud> feuilles) {
+		this.feuilles = feuilles;
+	}
+
+
+}
