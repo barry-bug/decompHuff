9c69364522d40c5337f8e8efdacdfa6ea9a82ff2
barry-bug <72456359+barry-bug@users.noreply.github.com>
2021-06-09 00:19:31 +0200

    Update Frequencechar.java

--- Frequencechar.java
+++ Frequencechar.java
@@ -1,11 +1,15 @@
+package decompHuff;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
 
 public class FrequenceChar {
-	private String text;  /*input*/
+	private String text; /*input*/
 	private Map<Character, Integer> frequences;/* les clés sont les caractères et les valeurs en integer sont les fréquences des caractères.*/
 	private List<Noeud> feuilles;  /* Les feuilles sont des listes de noeud: ils correspondent aux extrémités de l'arbre*/
 	
-	
-/*Constructeur et initialisation */
-	public FrequenceChar(String input) {
-		text = input;
-		
+	/*Constructeur et initialisation */
