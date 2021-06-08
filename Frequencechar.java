23ecf9d9bdc5c33565bf1a659a7702afbfe8d768
barry-bug <72456359+barry-bug@users.noreply.github.com>
2021-06-08 19:39:24 +0200

    Create Frequencechar.java

---
+++ Frequencechar.java
@@ -0,0 +1,6 @@
+
+public class FrequenceChar {
+	private String text; 
+	private Map<Character, Integer> frequences;/* les clés sont les caractères et les valeurs en integer sont les fréquences des caractères.*/
+	private List<Noeud> feuilles;  /* Les feuilles sont des listes de noeud: ils correspondent aux extrémités de l'arbre*/
+	
