42f847653bbc14c596941222269d12d5702e2b80
barry-bug <72456359+barry-bug@users.noreply.github.com>
2021-06-09 00:16:37 +0200

    Update noeud.java

--- noeud.java
+++ noeud.java
@@ -6,13 +6,12 @@ public class Noeud {
 
 	private int freq;
 	private Character caract;
-
+	private Noeud leftnode;
+	private Noeud rightnode;
 	
-	/*Constructeur de l'arbre de
-	 */
+	/*Constructeur du noeud*/
 	public Noeud(Character caract, int freq) {
 		this.caract = caract;
 		this.freq = freq;
 	
 }
-	
