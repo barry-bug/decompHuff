245b65b1a28b2f0677b1f6b2d43d46334bec3b90
barry-bug <72456359+barry-bug@users.noreply.github.com>
2021-06-08 19:47:11 +0200

    Update Frequencechar.java

--- Frequencechar.java
+++ Frequencechar.java
@@ -4,6 +4,8 @@ public class FrequenceChar {
 	private Map<Character, Integer> frequences;/* les clés sont les caractères et les valeurs en integer sont les fréquences des caractères.*/
 	private List<Noeud> feuilles;  /* Les feuilles sont des listes de noeud: ils correspondent aux extrémités de l'arbre*/
 	
+	
+/*Constructeur et initialisation */
 	public FrequenceChar(String input) {
 		text = input;
-		frequences
+		
